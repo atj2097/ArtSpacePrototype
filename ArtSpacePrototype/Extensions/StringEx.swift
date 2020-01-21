4c9996352ce610dd9ec71d61f5c3d9569f00c4aa
Adam <adamjackson@pursuit.org>
2020-01-21 11:55:25 -0500

    Added Folders

---
+++ ArtSpacePrototype/Extensions/StringEx.swift
@@ -0,0 +1,9 @@
+//
+//  StringEx.swift
+//  ArtSpacePrototype
+//
+//  Created by God on 1/21/20.
+//  Copyright © 2020 God. All rights reserved.
+//
+
+import Foundation
---
+++ ArtSpacePrototype/VCs/HomePageVC.swift
@@ -0,0 +1,30 @@
+//
+//  HomePageVC.swift
+//  ArtSpacePrototype
+//
+//  Created by God on 1/21/20.
+//  Copyright © 2020 God. All rights reserved.
+//
+
+import UIKit
+
+class HomePageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
