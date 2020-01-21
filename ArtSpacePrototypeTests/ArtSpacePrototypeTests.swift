a247f265ea9b893596d5c4092418e5452d67c80b
Adam <adamjackson@pursuit.org>
2020-01-21 11:35:24 -0500

    first commit

---
+++ ArtSpacePrototypeTests/ArtSpacePrototypeTests.swift
@@ -0,0 +1,34 @@
+//
+//  ArtSpacePrototypeTests.swift
+//  ArtSpacePrototypeTests
+//
+//  Created by God on 1/21/20.
+//  Copyright © 2020 God. All rights reserved.
+//
+
+import XCTest
+@testable import ArtSpacePrototype
+
+class ArtSpacePrototypeTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
