5e27722c1e33edfb2355a3b8e48dc31822be448f
Adam <adamjackson@pursuit.org>
2020-01-28 17:15:23 -0500

    Added post vc

--- ArtSpacePrototype/VCs/HomePageVC.swift
+++ ArtSpacePrototype/VCs/HomePageVC.swift
@@ -1,3 +1,4 @@
+
 //
 //  HomePageVC.swift
 //  ArtSpacePrototype
@@ -5,7 +6,6 @@
 //  Created by God on 1/21/20.
 //  Copyright © 2020 God. All rights reserved.
 //
-
 import UIKit
 import SnapKit
 var arrayOfImages = [UIImage(named: "1"),UIImage(named: "2"),UIImage(named: "3"),UIImage(named: "4"),UIImage(named: "5"),UIImage(named: "6"),UIImage(named: "7"),UIImage(named: "8"),UIImage(named: "9"),UIImage(named: "10")]
@@ -14,19 +14,27 @@ class HomePageVC: UIViewController {
     lazy var searchBar: UISearchBar = {
         let searchBar = UISearchBar()
         searchBar.placeholder = "Search"
+        searchBar.backgroundColor = .white
+        searchBar.barTintColor = .white
+        searchBar.tintColor = .white
         return searchBar
     }()
     
     lazy var filterButton: UIButton = {
         let button = UIButton()
         button.setImage(UIImage(systemName: "list.bullet"), for: .normal)
-
+        button.imageView?.contentMode = .scaleAspectFit
+        button.tintColor = .black
+        button.imageEdgeInsets = UIEdgeInsets(top: 25,left: 25,bottom: 25,right: 25)
         return button
     }()
     
-    lazy var mapView: UIButton = {
+    lazy var userProfile: UIButton = {
         let button = UIButton()
-        button.setImage(UIImage(systemName: "map"), for: .normal)
+        button.setImage(UIImage(systemName: "person"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+         button.imageEdgeInsets = UIEdgeInsets(top: 25,left: 25,bottom: 25,right: 25)
+        button.tintColor = .black
         return button
     }()
     
@@ -41,15 +49,38 @@ class HomePageVC: UIViewController {
         cv.dataSource = self
         return cv
     }()
+    
+    lazy var postButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.imageView?.contentMode = .scaleAspectFit
+        button.tintColor = .black
+        button.backgroundColor = .white
+        button.imageEdgeInsets = UIEdgeInsets(top: 25,left: 25,bottom: 25,right: 25)
+        button.addTarget(self, action: #selector(transitionToForm), for: .touchUpInside)
+        return button
+    }()
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
          self.navigationController?.navigationBar.isHidden = true
         self.view.backgroundColor = #colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)
-        [searchBar, filterButton, mapView,artCollectionView].forEach({self.view.addSubview($0)})
+        [searchBar, filterButton, userProfile,artCollectionView,postButton].forEach({self.view.addSubview($0)})
         setUpConst()
     }
     
     func setUpConst() {
+        
+        postButton.snp.makeConstraints { make in
+//            make.width.equalTo(50)
+//            make.height.equalTo(50)
+            make.bottom.equalTo(self.view).inset(100)
+            make.right.equalTo(searchBar).offset(50)
+            
+        }
+        
         searchBar.snp.makeConstraints { make in
             make.width.equalTo(200)
             make.top.equalTo(self.view).offset(75)
@@ -57,12 +88,12 @@ class HomePageVC: UIViewController {
         }
         
         filterButton.snp.makeConstraints { make in
-            make.top.equalTo(searchBar)
+            make.top.equalTo(searchBar).offset(15)
             make.right.equalTo(searchBar).offset(50)
         }
         
-        mapView.snp.makeConstraints { make in
-            make.top.equalTo(searchBar)
+        userProfile.snp.makeConstraints { make in
+            make.top.equalTo(searchBar).offset(15)
             make.left.equalTo(searchBar).offset(-50)
         }
         
@@ -74,6 +105,12 @@ class HomePageVC: UIViewController {
         }
         
         
+        
+    }
+    
+    @objc func transitionToForm() {
+        let nextVC = PostCreateForm()
+        self.navigationController?.pushViewController(nextVC, animated: true)
     }
 
 }
